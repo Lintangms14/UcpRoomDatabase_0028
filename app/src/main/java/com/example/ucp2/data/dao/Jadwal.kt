8abca8361181d4dca8747cba3b5a61b747a09583
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-18 17:30:31 +0700

    Membuat JadwalDao untuk CRUD

---
+++ app/src/main/java/com/example/ucp2/data/dao/Jadwal.kt
@@ -0,0 +1,27 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface Jadwal {
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
+    @Update
+    suspend fun updateJadwal(jadwal: Jadwal)
+
+    @Delete
+    suspend fun deleteJadwal(jadwal: Jadwal)
+
+    @Query("SELECT * FROM Jadwal ORDER BY namaPasien ASC")
+    fun getAllMahasiswa(): Flow<List<Jadwal>>
+
+    @Query("SELECT * FROM Jadwal WHERE id = :id")
+    fun getMahasiswa(id: String): Flow<Jadwal>
+}
