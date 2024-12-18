cbe0abf9e34154a156973d785f289ea3e639b946
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-18 17:23:54 +0700

    Dokterdao di gunakan untuk create dan read

---
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DokterDao {
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM Dokter ORDER BY nama ASC")
+    fun getAllDokte():Flow<List<Dokter>>
+}
