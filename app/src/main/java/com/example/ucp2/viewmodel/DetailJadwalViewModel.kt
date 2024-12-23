d6e320b365b91090948d1f3bea2bef5f798451d5
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-23 20:40:28 +0700

    untuk mengelola status pemuatan data

---
+++ app/src/main/java/com/example/ucp2/viewmodel/DetailJadwalViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.ucp2.viewmodel
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.repository.RepositoryJadwal
+import com.example.ucp2.ui.navigation.AlamatNavigation
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+data class DetailJadwalUiState(
+    val detailUiEvent: JadwalEvent = JadwalEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+){
+    val isUiEventEmpty: Boolean
+        get()= detailUiEvent == JadwalEvent()
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != JadwalEvent()
+}
