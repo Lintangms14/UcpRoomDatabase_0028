9e2c895d197c34dda0963f648c44acf5a9d97012
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-24 00:25:57 +0700

    untuk menampilkan informasi dokter yang berbentuk kartu

---
+++ app/src/main/java/com/example/ucp2/ui/view/dokter/HomeDokterView.kt
@@ -0,0 +1,135 @@
+package com.example.ucp2.ui.view.dokter
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.launch
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.R
+import com.example.ucp2.ui.customwidget.TopAppBar
+import com.example.ucp2.viewmodel.HomeDokterUiState
+import com.example.ucp2.viewmodel.HomeDokterViewModel
+import com.example.ucp2.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardDokter(
+    Dokter: Dokter,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+    val spesialisColor = when (Dokter.spesialis) {
+        "Dokter Spesialis Bedah" -> Color.Red
+        "Dokter Spesialis Syaraf" -> Color.Blue
+        "Dokter Spesialis Penyakit Dalam" -> Color.Green
+        "Dokter Spesialis Jantung" -> Color.Magenta
+        else -> Color.Gray
+    }
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding()
+            .clickable { onClick() }
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Person,
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    text = Dokter.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.DateRange,
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    text = Dokter.id,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Home,
+                    contentDescription = ""
+                )
+                Spacer(modifier = Modifier.padding(16.dp))
+                Text(
+                    text = Dokter.spesialis,
+                    fontWeight = FontWeight.Bold,
+                    color = spesialisColor
+                )
+            }
+        }
+    }
+}
+
