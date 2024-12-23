501427fbf3bf4843e57654e0fe8284a6fc33bef6
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-23 21:22:22 +0700

    untuk mengelola data jadwal, memvalidasi input, dan menyimpan data ke repositori

--- app/src/main/java/com/example/ucp2/viewmodel/JadwalViewModel.kt
+++ app/src/main/java/com/example/ucp2/viewmodel/JadwalViewModel.kt
@@ -43,3 +43,66 @@ data class FormErrorStateJadwal(
     }
 }
 
+data class JadwalUiState(
+    val jadwalEvent: JadwalEvent = JadwalEvent(),
+    val isEntryValid: FormErrorStateJadwal = FormErrorStateJadwal(),
+    val snackBarMessage: String? = null,
+)
+
+class JadwalViewModel(
+    private val repositoryJadwal: RepositoryJadwal
+) : ViewModel(){
+
+    var uiState by mutableStateOf(JadwalUiState())
+
+    fun updateState(jadwalEvent: JadwalEvent){
+        uiState = uiState.copy(
+            jadwalEvent = jadwalEvent,
+        )
+
+    }
+    private  fun validateFields() : Boolean{
+        val event = uiState.jadwalEvent
+        val errorState = FormErrorStateJadwal(
+            id = if (event.id.isNotEmpty()) "Id tidak boleh kosong" else null,
+            namaPasien = if (event.namaPasien.isNotEmpty()) "Nama Pasien tidak boleh kosong" else null,
+            namaDokter = if (event.namaDokter.isNotEmpty()) "Nama Dokter tidak boleh kosong" else null,
+            noTelpon = if (event.noTelpon.isNotEmpty()) "noTelepon tidak boleh kosong" else null,
+            tanggalKonsultasi = if (event.tanggalKonsultasi.isNotEmpty()) "Tanggal Konsultasi tidak boleh kosong" else null,
+            status = if (event.status.isNotEmpty()) "Status tidak boleh kosong" else null
+        )
+        uiState = uiState.copy(isEntryValid = errorState)
+        return errorState.isValid()
+    }
+
+    fun saveData() {
+
+        val currentEvent = uiState.jadwalEvent
+
+        if (validateFields()) {
+            viewModelScope.launch {
+                try {
+                    repositoryJadwal.insertJadwal(currentEvent.toJadwalEntity())
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data berhasil disimpan",
+                        jadwalEvent = JadwalEvent(),
+                        isEntryValid = FormErrorStateJadwal()
+                    )
+                } catch (e: Exception) {
+                    uiState = uiState.copy(
+                        snackBarMessage = "Data gagal disimpan"
+                    )
+                }
+            }
+        }else {
+            uiState = uiState.copy(
+                snackBarMessage = "Data tidak valid. Periksa kembali data Anda."
+            )
+        }
+    }
+    fun resetSnackBarMessage() {
+        uiState = uiState.copy(
+            snackBarMessage = null
+        )
+    }
+}
