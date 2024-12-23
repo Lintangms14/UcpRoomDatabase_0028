4d97c2ef85efe7870d3c100bcb1ab1ca13378b8c
Lintangms14 <merayusukmalintang@gmail.com>
2024-12-24 00:19:56 +0700

    digunakan untuk membuat form input jadwal

---
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/InsertJdwlView.kt
@@ -0,0 +1,113 @@
+package com.example.ucp2.ui.view.jadwal
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.navigation.AlamatNavigation
+import com.example.ucp2.ui.customwidget.TopAppBar
+import com.example.ucp2.viewmodel.*
+import kotlinx.coroutines.launch
+
+@Preview(showBackground = true)
+@Composable
+fun FormJadwal(
+    jadwalEvent: JadwalEvent = JadwalEvent(),
+    onValueChange: (JadwalEvent) -> Unit = {},
+    errorState: FormErrorStateJadwal = FormErrorStateJadwal(),
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = jadwalEvent.id,
+            onValueChange = {
+                onValueChange(jadwalEvent.copy(id = it))
+            },
+            label = { Text(text = "ID") },
+            isError = errorState.id != null
+        )
+        Text(
+            text = errorState.id ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = jadwalEvent.namaDokter,
+            onValueChange = {
+                onValueChange(jadwalEvent.copy(namaDokter = it))
+            },
+            label = { Text(text = "Nama Dokter") },
+            isError = errorState.namaDokter != null
+        )
+        Text(
+            text = errorState.namaDokter ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = jadwalEvent.namaPasien,
+            onValueChange = {
+                onValueChange(jadwalEvent.copy(namaPasien = it))
+            },
+            label = { Text(text = "Nama Pasien") },
+            isError = errorState.namaPasien != null
+        )
+        Text(
+            text = errorState.namaPasien ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = jadwalEvent.noTelpon,
+            onValueChange = {
+                onValueChange(jadwalEvent.copy(noTelpon = it))
+            },
+            label = { Text(text = "No Telepon") },
+            isError = errorState.noTelpon != null
+        )
+        Text(
+            text = errorState.noTelpon ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = jadwalEvent.tanggalKonsultasi,
+            onValueChange = {
+                onValueChange(jadwalEvent.copy(tanggalKonsultasi = it))
+            },
+            label = { Text(text = "Tanggal Konsultasi") },
+            isError = errorState.tanggalKonsultasi != null
+        )
+        Text(
+            text = errorState.tanggalKonsultasi ?: "",
+            color = Color.Red
+        )
+
+        OutlinedTextField(
+            modifier = modifier.fillMaxWidth(),
+            value = jadwalEvent.status,
+            onValueChange = {
+                onValueChange(jadwalEvent.copy(status = it))
+            },
+            label = { Text(text = "Status") },
+            isError = errorState.status != null
+        )
+        Text(
+            text = errorState.status ?: "",
+            color = Color.Red
+        )
+    }
+}
+
